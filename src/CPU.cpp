84d86f2ef857ef95eab6e61460c49207f2c797c7
cjanzen <cjanzen@gmail.com>
2019-07-12 19:35:32 -0700

    Implement instructions up to CMP

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -5,11 +5,11 @@
 
 CPU::CPU(const char* file)
 {
-	registers.P = 0x34;
-	registers.A = 0;
+	registers.SR = 0x34;
+	registers.AC = 0;
 	registers.X = 0;
 	registers.Y = 0;
-	registers.S = 0xFD;
+	registers.SP = 0xFD;
 	registers.PC = 0; //This might need to be changed based on mapper
 	
 	memory[0x4017] = 0x00;
@@ -131,69 +131,88 @@ uint8_t CPU::readROM()
 	return memory[mappedPC];
 }
 
+uint8_t CPU::pop()
+{
+	++registers.SP;
+	return readMEMORY(registers.SP);
+}
+void CPU::push(uint8_t data)
+{
+	writeMEMORY(registers.SP, data);
+	++registers.SP;
+}
+
 bool CPU::if_carry()
 {
-	return registers.P & 0x1;
+	return registers.SR & 0x1;
 }
 
 bool CPU::if_overflow()
 {
-	return (registers.P >> 6) & 0x1;
+	return (registers.SR >> 6) & 0x1;
 }
 
 bool CPU::if_sign()
 {
-	return (registers.P >> 7) & 0x1;
+	return (registers.SR >> 7) & 0x1;
 }
 
 bool CPU::if_zero()
 {
-	return (registers.P >> 1) & 0x1;
-}
-
-void CPU::set_sign(uint16_t value)
-{
-	registers.P |= ((value >> 7) & 0x1) << 7;
-}
-
-void CPU::set_zero(uint16_t value)
-{
-	if(value == 0)
-		registers.P |= 0x1 << 1;
-	else
-		registers.P &= ~(0x1 << 1);
+	return (registers.SR >> 1) & 0x1;
 }
 
 void CPU::set_carry(bool condition)
 {
 	if(condition)
-		registers.P |= 0x1;
+		registers.SR |= 0x1;
 	else
-		registers.P &= ~(0x1);
+		registers.SR &= ~(0x1);
 }
 
-void CPU::set_overflow(bool condition)
+void CPU::set_zero(uint16_t value)
 {
-	if(condition)
-		registers.P |= 0x1 << 6;
+	if(value == 0)
+		registers.SR |= 0x1 << 1;
 	else
-		registers.P &= ~(0x1 << 6);
+		registers.SR &= ~(0x1 << 1);
 }
 
 void CPU::set_interrupt(bool condition)
 {
 	if(condition)
-		registers.P |= 0x1 << 2;
+		registers.SR |= 0x1 << 2;
 	else
-		registers.P &= ~(0x1 << 2);
+		registers.SR &= ~(0x1 << 2);
+}
+
+void CPU::set_decimal(bool condition)
+{
+	if(condition)
+		registers.SR |= 0x1 << 3;
+	else
+		registers.SR &= ~(0x1 << 3);
 }
 
 void CPU::set_break(bool condition)
 {
 	if(condition)
-		registers.P |= 0x1 << 4;
+		registers.SR |= 0x1 << 4;
 	else
-		registers.P &= ~(0x1 << 4);
+		registers.SR &= ~(0x1 << 4);
+}
+
+void CPU::set_overflow(bool condition)
+{
+	if(condition)
+		registers.SR |= 0x1 << 6;
+	else
+		registers.SR &= ~(0x1 << 6);
+}
+
+void CPU::set_sign(uint16_t value)
+{
+	registers.SR |= ((value >> 7) & 0x1) << 7;
 }
 
 uint16_t CPU::fetchZeroPageAddress()
@@ -285,13 +304,13 @@ void CPU::executeInstruction()
 			ADC(fetchZeroPageXAddress());
 			break;
 		}
-		case 0x60: //Absolute ADC
+		case 0x6D: //Absolute ADC
 		{
 			cycles = 4;
 			ADC(fetchAbsoluteAddress());
 			break;
 		}
-		case 0x70: //Absolute,X ADC
+		case 0x7D: //Absolute,X ADC
 		{
 			cycles = 4;
 			ADC(fetchAbsoluteXAddress());
@@ -366,7 +385,7 @@ void CPU::executeInstruction()
 		case 0x0A: //Accumulator ASL
 		{
 			cycles = 2;
-			ASL(registers.A);
+			ASL(registers.AC);
 			break;
 		}
 		case 0x06: //Zero Page ASL
@@ -395,41 +414,203 @@ void CPU::executeInstruction()
 		}
 		case 0x90: //Relative BCC
 		{
+			BRANCH(!if_carry());
+			break;
+		}
+		case 0xB0: //Relative BCS
+		{
+			BRANCH(if_carry());
+			break;
+		}
+		case 0xF0: //Relative BEQ
+		{
+			BRANCH(if_zero());
+			break;
+		}
+		case 0x24: //Zero Page BIT
+		{
+			cycles = 3;
+			BIT(fetchZeroPageAddress());
+			break;
+		}
+		case 0x2C: //Absolute BIT
+		{
+			cycles = 4;
+			BIT(fetchAbsoluteAddress());
+			break;
+		}
+		case 0x30: //Relative BMI
+		{
+			BRANCH(if_sign());
+			break;
+		}
+		case 0xD0: //Relative BNE
+		{
+			BRANCH(!if_zero());
+			break;
+		}
+		case 0x10: //Relative BPL
+		{
+			BRANCH(!if_sign());
+			break;
+		}
+		case 0x00: //Implied BRK
+		{
+			cycles = 7;
+			++registers.PC;
+			push((registers.PC >> 8) & 0xFF);
+			push(registers.PC & 0xFF);
+			set_break(1);
+			push(registers.SR);
+			set_interrupt(1);
+			uint16_t newPC = readMEMORY(0xFFFF);
+			newPC <<= 8;
+			newPC += readMEMORY(0xFFFE);
+			registers.PC = newPC;
+			break;
+		}
+		case 0x50: //Relative BVC
+		{
+			BRANCH(!if_overflow());
+			break;
+		}
+		case 0x70: //Relative BVS
+		{
+			BRANCH(if_overflow());
+			break;
+		}
+		case 0x18: //Implied CLC
+		{
 			cycles = 2;
-			BCC(readROM());
+			set_carry(0);
+			break;
+		}
+		case 0xD8: //Implied CLD
+		{
+			cycles = 2;
+			set_decimal(0);
+			break;
+		}
+		case 0x58: //Implied CLI
+		{
+			cycles = 2;
+			set_interrupt(0);
+			break;
+		}
+		case 0xB8: //Implied CLV
+		{
+			cycles = 2;
+			set_overflow(0);
+			break;
+		}
+		case 0xC9: //Immediate CMP
+		{
+			cycles = 2;
+			CMP(readROM());
+			break;
+		}
+		case 0xC5: //Zero Page CMP
+		{
+			cycles = 3;
+			CMP(fetchZeroPageAddress());
+			break;
+		}
+		case 0xD5: //Zero Page,X CMP
+		{
+			cycles = 4;
+			CMP(fetchZeroPageXAddress());
+			break;
+		}
+		case 0xCD: //Absolute CMP
+		{
+			cycles = 4;
+			CMP(fetchAbsoluteAddress());
+			break;
+		}
+		case 0xDD: //Absolute,X CMP
+		{
+			cycles = 4;
+			CMP(fetchAbsoluteXAddress());
+			break;
+		}
+		case 0xD9: //Absolute,Y CMP
+		{
+			cycles = 4;
+			CMP(fetchAbsoluteYAddress());
+			break;
+		}
+		case 0xC1: //Indirect,X CMP
+		{
+			cycles = 6;
+			CMP(fetchIndirectXAddress());
+			break;
+		}
+		case 0xD1: //Indirect,Y CMP
+		{
+			cycles = 5;
+			CMP(fetchIndirectYAddress());
 			break;
 		}
 	}
 }
 
+uint16_t CPU::relativeAddress(uint8_t offset)
+{
+	bool isNegative = (offset & 0x80) >> 7;
+	if(isNegative)
+	{
+		int8_t signedOffset = offset & 0x7F;
+		signedOffset *= -1;
+		return registers.PC + signedOffset;
+	}
+	return registers.PC + offset;
+}
+
 void CPU::ADC(uint8_t operand)
 {
-	uint16_t temp = operand + registers.A + (if_carry() ? 1 : 0);
+	uint16_t temp = operand + registers.AC + (if_carry() ? 1 : 0);
 	set_zero(temp & 0xFF);
 	set_sign(temp);
-	set_overflow(!((registers.A ^ operand) & 0x80) && ((registers.A ^ temp) & 0x80));
+	set_overflow(!((registers.AC ^ operand) & 0x80) && ((registers.AC ^ temp) & 0x80));
 	set_carry(temp > 0xFF);
-	registers.A = temp;
+	registers.AC = temp;
 }
 
 void CPU::ADC(uint16_t operandAddress)
 {
 	uint8_t operand = readMEMORY(operandAddress);
-	uint16_t temp = operand + registers.A + (if_carry() ? 1 : 0);
+	uint16_t temp = operand + registers.AC + (if_carry() ? 1 : 0);
 	set_zero(temp & 0xFF);
 	set_sign(temp);
-	set_overflow(!((registers.A ^ operand) & 0x80) && ((registers.A ^ temp) & 0x80));
+	set_overflow(!((registers.AC ^ operand) & 0x80) && ((registers.AC ^ temp) & 0x80));
 	set_carry(temp > 0xFF);
-	registers.A = temp;
+	registers.AC = temp;
+}
+
+void CPU::AND(uint8_t operand)
+{
+	operand &= registers.AC;
+	set_sign(operand);
+	set_zero(operand);
+	registers.AC = operand;
 }
 
 void CPU::AND(uint16_t operandAddress)
 {
 	uint8_t operand = readMEMORY(operandAddress);
-	operand &= registers.A;
+	operand &= registers.AC;
 	set_sign(operand);
 	set_zero(operand);
-	registers.A = operand;
+	registers.AC = operand;
+}
+
+void CPU::ASL(uint8_t operand)
+{
+	set_carry(operand & 0x80);
+	operand <<= 1;
+	set_sign(operand);
+	set_zero(operand);
+	registers.AC = operand;
 }
 
 void CPU::ASL(uint16_t operandAddress)
@@ -439,18 +620,46 @@ void CPU::ASL(uint16_t operandAddress)
 	operand <<= 1;
 	set_sign(operand);
 	set_zero(operand);
+	writeMEMORY(operandAddress, operand);
 }
 
-void CPU::BCC(int8_t operand)
+void CPU::BIT(uint16_t operandAddress)
 {
-	
+	uint8_t operand = readMEMORY(operandAddress);
+	set_sign(operand);
+	set_overflow(0x40 & operand);
+	set_zero(operand & registers.AC);
+}
+
+void CPU::BRANCH(bool condition)
+{
+	cycles = 2;
+	uint8_t operand = readROM();
+	if(condition)
+	{
+		uint16_t newPC = relativeAddress(operand);
+		cycles += ((registers.PC & 0xFF00) != (newPC & 0xFF00) ? 2 : 1);
+		registers.PC = newPC;
+	}
+}
+
+void CPU::CMP(uint8_t operand)
+{
+	uint16_t result = registers.AC - operand;
+	set_carry(result < 0x100);
+	set_sign(result);
+	set_zero(result & 0xFF);
+}
+
+void CPU::CMP(uint16_t operandAddress)
+{
+	uint8_t operand = readMEMORY(operandAddress);
+	uint16_t result = registers.AC - operand;
+	set_carry(result < 0x100);
+	set_sign(result);
+	set_zero(result & 0xFF);
 }
 
 void CPU::CPU_TESTING()
 {
-	uint8_t a = 0x69;
-	uint16_t b = 0x6969;
-	ADC(a);
-	ADC(b);
-	//std::cout << std::hex << (uint)read(0x0869) << std::endl;
 }
--- src/CPU.hpp
+++ src/CPU.hpp
@@ -17,12 +17,12 @@ private:
 
 	struct CPU_Registers
 	{
-		uint8_t A;		//Accumulator
+		uint8_t AC;		//Accumulator
 		uint8_t X;		//X
 		uint8_t Y;		//Y
 		uint16_t PC;	//Program Counter
-		uint8_t S;		//Stack Pointer
-		uint8_t P;		//Status
+		uint8_t SP;		//Stack Pointer
+		uint8_t SR;		//Status
 	};
 
 	CPU_Registers registers;
@@ -38,18 +38,21 @@ private:
 	uint8_t readMEMORY(uint16_t address);
 	void writeMEMORY(uint16_t address, uint8_t data);
 	uint8_t readROM();
+	uint8_t pop();
+	void push(uint8_t);
 
 	//Used to read/set status register bits
 	bool if_carry();
 	bool if_overflow();
 	bool if_sign();
 	bool if_zero();
-	void set_sign(uint16_t value);
-	void set_zero(uint16_t value);
 	void set_carry(bool condition);
-	void set_overflow(bool condition);
+	void set_zero(uint16_t value);
 	void set_interrupt(bool condition);
+	void set_decimal(bool condition);
 	void set_break(bool condition);
+	void set_overflow(bool condition);
+	void set_sign(uint16_t value);
 
 	//Fetch operand address
 	uint16_t fetchImmediateAddress();
@@ -65,14 +68,19 @@ private:
 	void executeInstruction();
 
 	//Utility
-	uint16_t relativeAddress(int8_t )
+	uint16_t relativeAddress(uint8_t offset);
 
 	//Instructions
 	void ADC(uint8_t operand);
 	void ADC(uint16_t operandAddress);
+	void AND(uint8_t operand);
 	void AND(uint16_t operandAddress);
+	void ASL(uint8_t operand);
 	void ASL(uint16_t operandAddress);
-	void BCC(int8_t operand);
+	void BIT(uint16_t operandAddress);
+	void BRANCH(bool condition);
+	void CMP(uint8_t operand);
+	void CMP(uint16_t operandAddress);
 };
 
 #endif
